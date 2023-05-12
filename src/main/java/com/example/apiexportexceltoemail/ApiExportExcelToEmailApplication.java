ebf279e30b5f84497ba5796361a74e196faede37
MartinDiCe <jamartindice@gmail.com>
2023-05-12 11:42:45 -0300

    creando rpeo

---
+++ src/main/java/com/example/apiexportexceltoemail/ApiExportExcelToEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiexportexceltoemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExportExcelToEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExportExcelToEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiexportexceltoemail/ApiExportExcelToEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiexportexceltoemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExportExcelToEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
