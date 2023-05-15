7615c61188bc59efbd8a5fd9c530458069122e6c
MartinDiCe <jamartindice@gmail.com>
2023-05-15 18:55:39 -0300

    Corrigiendo service implement

--- src/main/java/com/example/apiexportexceltoemail/service/GetReportServiceImplement.java
+++ src/main/java/com/example/apiexportexceltoemail/service/GetReportServiceImplement.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-public class GetReportServiceImplement {
+public class GetReportServiceImplement implements GetReportService {
 
     private final ReportRepository repository;
 
